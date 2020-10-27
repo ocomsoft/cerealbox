bdffd2911bde90db0f9778114add29bc22db7357
Scott Warren <scottwarren@ocom.com.au>
2020-10-28 10:21:58 +1100

    Rename files

--- cerealbox_tomap.go
+++ cerealbox_tomap.go
@@ -7,20 +7,6 @@ import (
 	"time"
 )
 
-type SerializerFunc func(builder ISerializer) ISerializer
-
-type ISerializable interface {
-	Serialize(builder ISerializer) ISerializer
-}
-
-type ISerializer interface {
-	DoInt(string, string, bool, int, int) ISerializer
-	DoFloat64(string, string, bool, float64, float64) ISerializer
-	DoString(string, string, bool, int, int) ISerializer
-	DoBool(string, string, bool) ISerializer
-	DoTime(string, string, bool, *time.Time, *time.Time) ISerializer
-}
-
 type SerializerToMap struct {
 	result map[string]interface{}
 	errors map[string]error
